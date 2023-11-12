3c7cae7ada178dfd226e7d9066e8eebecf1b95db
Aditfrm <aditya.fiirmansyah@gmail.com>
2023-11-12 21:52:45 +0700

    init commit

--- models/historyModel.js
+++ models/historyModel.js
@@ -1,22 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const historySchema = new mongoose.Schema({
-    userId: {
-        type: Number,
-        required: true
-    },
-    applicationDate: {
-        type: Number,
-        required: true
-    },
-    franchiseId: {
-        type: Number,
-        required: true
-    },
-    status: {
-        type: Boolean,
-        required: true
-    }
-})
+  userId: {
+    type: Number,
+    required: true,
+  },
+  applicationDate: {
+    type: Number,
+    required: true,
+  },
+  franchiseId: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    required: true,
+  },
+  franchiseeID: {
+    type: Number,
+    required: true,
+  },
+  franchiseeName: {
+    type: String,
+    required: true,
+  },
+  franchiseeAddress: {
+    type: String,
+    required: true,
+  },
+  franchiseePlaceDateBirth: {
+    type: Number,
+    required: true,
+  },
+  franchiseePhoneNumber: {
+    type: Number,
+    required: true,
+  },
+  franchiseName: {
+    type: String,
+    required: true,
+  },
+  franchiseAddress: {
+    type: String,
+    required: true,
+  },
+  franchisePhoneNumber: {
+    type: Number,
+    required: true,
+  },
+  fundingCapital: {
+    type: Number,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('histories', historySchema, 'history');
+module.exports = mongoose.model("histories", historySchema, "history");
--- routes/routes.js
+++ routes/routes.js
@@ -1,33 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {getHistory} = require('../controller/historyController');
-const {registerUser, getUserProfile, getTokenAuth } = require('../controller/userController');
-const { getAllFranchise, getOneFranchise } = require('../controller/franchiseController');
+const { getHistory } = require("../controller/historyController");
+const {
+  registerUser,
+  getUserProfile,
+  getTokenAuth,
+} = require("../controller/userController");
+const {
+  getAllFranchise,
+  getOneFranchise,
+  applyFranchise,
+} = require("../controller/franchiseController");
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
+router.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-router.get('/:userId/history', getHistory);
+router.get("/:userId/history", getHistory);
 
-router.get('/:userId/profile', async (req, res) =>  {
-    await getUserProfile(req, res);
+router.get("/:userId/profile", async (req, res) => {
+  await getUserProfile(req, res);
 });
 
-router.get('/signin', async (req, res) => {
-    await getTokenAuth(req, res);
+router.get("/signin", async (req, res) => {
+  await getTokenAuth(req, res);
 });
 
-router.post('/signup', async (req, res) => {
-    await registerUser(req, res);
+router.post("/signup", async (req, res) => {
+  await registerUser(req, res);
 });
 
-router.get('/franchises', async (req, res) => {
-    await getAllFranchise(req, res);
+router.get("/franchises", async (req, res) => {
+  await getAllFranchise(req, res);
 });
 
-router.get('/franchises/:franchiseId', async (req, res) => {
-    await getOneFranchise(req, res);
-})
+router.get("/franchises/:franchiseId", async (req, res) => {
+  await getOneFranchise(req, res);
+});
 
+router.post("/franchises/apply", async (req, res) => {
+  await applyFranchise(req, res);
+});
 module.exports = router;
